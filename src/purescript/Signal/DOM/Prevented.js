eab6b37b8198ddac79f434703ae6290bc161223c
Mikołaj Biel <mbiel@mailbox.org>
2021-11-19 21:55:38 +0100

    Fix prevented keypress events

--- src/purescript/Signal/DOM/Prevented.js
+++ src/purescript/Signal/DOM/Prevented.js
@@ -8,12 +8,16 @@ exports.keyPressedPWithPrevent =
 				return function() {
 					var out = constant(false);
 					window.addEventListener("keydown", function(e) {
-						if (e.keyCode === keyCode) out.set(true);
-						if (preventDefault) e.preventDefault();
+						if (e.keyCode === keyCode) {
+							out.set(true);
+							if (preventDefault) e.preventDefault();
+						}
 					});
 					window.addEventListener("keyup", function(e) {
-						if (e.keyCode === keyCode) out.set(false);
-						if (preventDefault) e.preventDefault();
+						if (e.keyCode === keyCode) {
+							out.set(false); 
+							if (preventDefault) e.preventDefault();
+						}
 					});
 					return out;
 				};
