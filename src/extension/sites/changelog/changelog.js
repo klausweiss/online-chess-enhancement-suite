22c01aa6aec5c46522059eae9c673536538561cc
Mikołaj Biel <mbiel@mailbox.org>
2021-11-30 20:25:17 +0100

    Hide changelog if there are no relevant changes

--- src/extension/sites/changelog/changelog.js
+++ src/extension/sites/changelog/changelog.js
@@ -1,14 +1,26 @@
 window.onload = function() {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  if (!urlParams.has("from")) return;
-  const from = urlParams.get("from");
+  const from = urlParams.get("from") || "0.0.0";
 
   var seenAllRelevantChanges = false;
+  var shouldRemoveUntilNextH2 = false;
   document.querySelectorAll("body *").forEach((el) => {
     seenAllRelevantChanges |= (el.firstElementChild && el.firstElementChild.text == from);
-    if (seenAllRelevantChanges) el.remove();
+    if (el.tagName == "H2") {
+      if (el.nextElementSibling && el.nextElementSibling.querySelector("a[href='#ignore-in-changelog-within-extension']")) {
+        shouldRemoveUntilNextH2 = true;
+      } else {
+        shouldRemoveUntilNextH2 = false;
+      }
+    }
+    if (seenAllRelevantChanges || shouldRemoveUntilNextH2) el.remove();
   });
+
+  if (!document.querySelector("h2")) {
+    // no headers left
+    window.close();
+  }
 
   document.querySelector("h1").innerText = document.title;
 };
